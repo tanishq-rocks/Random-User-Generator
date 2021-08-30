8ccaf73dd563d3f9377955a599093c5f8bb03222
Tanishqqjain <tanishqlearn@gmail.com>
2021-08-31 01:33:27 +0530

    App.vue updated

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <article :class="[(this.gender === 'female' ? 'b--pink' : 'b--blue'),'mw5', 'center', 'bg-white', 'br3', 'pa3', 'pa4-ns', 'mv3', 'ba', 'bw2']">
+    <div class="tc">
+      <img :src="photo" :class="[(this.gender === 'female' ? 'b--pink' : 'b--blue'),'br-100', 'h4', 'w4', 'dib', 'ba', 'bw2', 'pa2']" title="Random User">
+      <h1 class="f3 mb2">{{username}}</h1>
+      <div :class="[(this.gender === 'female' ? 'b--pink' : 'b--blue'), 'ba', 'b--dashed', 'br-pill', 'bw1']"></div>
+      <br>
+      <h2 class="f6 fw2 gray mt0">{{phone}}</h2>
+      <h2 class="f6 fw2 gray mt0 flex-wrap">{{email}}</h2>
+    </div>
+  </article>
+  <button @click="onGenerate" class="f6 grow no-underline br-pill ph3 pv2 mb2 di bg-white w-25">Generate</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      username: 'Cat',
+      phone: 1234567890,
+      email: 'lovelycat@gmail.com',
+      photo: 'http://tachyons.io/img/avatar_1.jpg',
+      gender: 'female'
+    }
+  },
+  methods: {
+    onGenerate(){
+      fetch('https://randomuser.me/api/')
+        .then(response => response.json())
+        .then(
+          (data) => {
+            this.username = data.results[0].name.first + data.results[0].name.last;
+            this.phone = data.results[0].phone;
+            this.email = data.results[0].email;
+            this.photo = data.results[0].picture.medium;
+            this.gender = data.results[0].gender;
+          }
+        )
+    }
+  },
 }
 </script>
 
@@ -21,6 +49,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
